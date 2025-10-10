2e9427a0a02eefead62391e747da690f449e069e
mrithip <mrithip2005@gmail.com>
2025-10-10 15:53:51 +0530

    recent

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Login");
+let ob1 = { age:33};
+let ob2 = ob1;
+ob1.age = 34;
+console.log(ob2.age); // Output: 34
+console.log(9&6);
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(2,3));
+const sum2=(a,b,c)=>{
+    console.log(a+b+c);
+}
+
